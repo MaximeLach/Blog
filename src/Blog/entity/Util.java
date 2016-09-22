4fb5a84db816a4351ab54c385052e2684a3c9018
MaximeLach <maxime.lach@gmail.com>
2016-09-22 16:49:15 +0200

    Blog v1

---
+++ src/Blog/entity/Util.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Blog.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author admin
+ */
+@Entity
+public class Util implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @OneToMany(mappedBy = "util")
+    private List<Page> pages = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "util")
+    private List<Article> articles = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "util")
+    private List<Commentaire> commentaires = new ArrayList<>();
+
+    @OneToMany(mappedBy = "utilexpediteur")
+    private List<Message> messagesEnvoyes = new ArrayList<>();
+    
+    @OneToMany(mappedBy = "utildestinataire")
+    private List<Message> messagesRecus = new ArrayList<>();
+    
+    @OneToOne
+    @JoinColumn(name = "NumSecu_id")
+    private NumSecu numsecu;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Util)) {
+            return false;
+        }
+        Util other = (Util) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Blog.entity.Util[ id=" + id + " ]";
+    }
+    
+}
